369d72968561883b893a80db79d8ccaf7325f673
Travis T <travistokuyama@gmail.com>
2018-09-26 19:06:44 +0000

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,10 @@
 class Cat
-  attr_accessor :name
+  attr_reader :name
+  attr_accessor :mood
+  
+  def initialize(name, mood = "nervous")
+    @name = name
+    @mood = mood
+  end
+  
 end
--- lib/fish.rb
+++ lib/fish.rb
@@ -1,3 +1,10 @@
 class Fish
-  attr_accessor :name
+  attr_reader :name
+  attr_accessor :mood
+  
+  def initialize(name, mood = "nervous")
+    @name = name
+    @mood = mood
+  end
+  
 end
